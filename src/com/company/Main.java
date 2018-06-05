ce7a5a8c23d825dc29c9aef9c47d958feed00ee8
Jared-See <pagaley12@outlook.com>
2018-06-04 21:30:42 -0400

    Countinued trying to implement BigIntegers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,14 +12,14 @@ public class Main {
     public static void factorize(BigInteger n){
         int factorA = 0;
         int factorB = 0;
-        BigInteger aChecker = new BigInteger("2");
-        int checkerPartTwo = n.compareTo(aChecker);
+        BigInteger Checker = new BigInteger("2");                    //A Variable to decide if the number is even because you can't direct compare a int to a BigInt, size matters
+        int checkerPartTwo = n.compareTo(Checker);
 
         if (checkerPartTwo == 0){                                       //If number is even
             factorA = 2;
             factorB = n.divide(2);
         }else{                                                          //If number is odd
-            int halfLine = n/2;                                         //Only variables that are even will be factors greater than half of them, this case is only for odd integers
+            BigInteger halfLine = new BigInteger(n.divide(/* Some representation of the number 2*/)) ;   //Only variables that are even will be factors greater than half of them, this case is only for odd integers
             int smallestFactorOdd = 3;
 
             while (smallestFactorOdd < halfLine){                       //Until you've found the smallest factor for add variables
@@ -32,7 +32,7 @@ public class Main {
                 }
             }
 
-            if (smallestFactorOdd > halfLine){                          //If the number isnt even and has no numbers that multiply together that are natural
+            if (smallestFactorOdd > halfLine){                          //If the number isn't even and has no numbers that multiply together that are natural
                 System.out.println("Prime Number");                     //If the above is true then the number is prime
             }
         }
